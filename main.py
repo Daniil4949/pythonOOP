75c0a8d87ea69a0734f01e8c056ff8d74438c78f
Daniil Kimstach <daniilkimstachp@gmail.com>
2022-05-19 09:13:07 +0300

    Property methods were added.

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-class Cat:
-    __shared_attr = {
-        'breed': 'pers',
-        'color': 'black'
-    }
+class BankAccpount:
+    def __init__(self, name, balance):
+        self.name = name
+        self.__balance = balance
 
-    def __init__(self):
-        self.__dict__ = Cat.__shared_attr
+    def get_balance(self):
+        return self.__balance
+
+    def set_balance(self, value):
+        if not isinstance(value, (int, float)):
+            raise ValueError('Balance must be a number')
+
+        self.__balance = value
+
+    def delete_balance(self):
+        del self.__balance
+
+    my_balance = property(get_balance, set_balance, delete_balance)
