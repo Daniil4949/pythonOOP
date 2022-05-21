9f2c2d94c25195d345ebdd1cc144695da8163f63
Daniil Kimstach <daniilkimstachp@gmail.com>
2022-05-21 11:40:50 +0300

    New methods were added.

--- main.py
+++ main.py
@@ -90,33 +90,43 @@
 # print(s.area())
 # print(r.area())
 
-class Square:
-    def __init__(self, s):
-        self.__side = s
-        self.__area = None
-        self.__perimeter = None
+# class Square:
+#     def __init__(self, s):
+#         self.__side = s
+#         self.__area = None
+#         self.__perimeter = None
+#
+#     @property
+#     def side(self):
+#         return self.__side
+#
+#     @side.setter
+#     def side(self, value):
+#         self.__side = value
+#         self.__area = None
+#
+#     @property
+#     def area(self):
+#         if self.__area is None:
+#             self.__area = self.__side ** 2
+#         return self.__area
+#
+#     @property
+#     def perimeter(self):
+#         if self.__perimeter is None:
+#             self.__perimeter = self.__side * 4
+#         return self.__perimeter
+#
+#
+# r = Square(2)
+# print(r.area)
 
-    @property
-    def side(self):
-        return self.__side
 
-    @side.setter
-    def side(self, value):
-        self.__side = value
-        self.__area = None
+class DepartmentIT:
 
-    @property
-    def area(self):
-        if self.__area is None:
-            self.__area = self.__side ** 2
-        return self.__area
+    PYTHON_DEV = 3
+    GO_DEV = 3
+    REACT_DEV = 2
 
-    @property
-    def perimeter(self):
-        if self.__perimeter is None:
-            self.__perimeter = self.__side * 4
-        return self.__perimeter
-
-
-r = Square(2)
-print(r.area)
+    def info(self):
+        print(DepartmentIT.GO_DEV)
