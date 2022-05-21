50f523f1f63e8956badf37ae97cc89a10e1f845b
Daniil Kimstach <daniilkimstachp@gmail.com>
2022-05-21 12:33:11 +0300

    New magic methods were added.

--- main.py
+++ main.py
@@ -122,11 +122,33 @@
 # print(r.area)
 
 
-class DepartmentIT:
+# class DepartmentIT:
+#
+#     PYTHON_DEV = 3
+#     GO_DEV = 3
+#     REACT_DEV = 2
+#
+#     def info(self):
+#         print(DepartmentIT.GO_DEV)
 
-    PYTHON_DEV = 3
-    GO_DEV = 3
-    REACT_DEV = 2
+class BankAccount:
 
-    def info(self):
-        print(DepartmentIT.GO_DEV)
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+
+    def __add__(self, other):
+        print("__add__ was declared.")
+        if isinstance(other, BankAccount):
+            return self.balance + other.balance
+        if isinstance(other, (int, float)):
+            return self.balance + other
+        raise NotImplementedError('This operation is not supported.')
+
+    def __radd__(self, other):
+        return self + other
+
+
+b = BankAccount('Misha', 20)
+c = BankAccount('Slava', 23)
+print(13 + b)
