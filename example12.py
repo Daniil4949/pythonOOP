6400926bc98f4b4556b495265a433e9c8280ba70
Daniil Kimstach <daniilkimstachp@gmail.com>
2022-05-28 19:19:41 +0300

    New __str__ and __bool__ methods were added.

--- example12.py
+++ example12.py
@@ -63,20 +63,46 @@
 # print(magnus < ian) # False
 # print(magnus < [1, 2]) # печатает "Невозможно выполнить сравнениe"
 
-class City:
-    def __init__(self, name: str):
-        self.name = name.title()
+# class City:
+#     def __init__(self, name: str):
+#         self.name = name.title()
+#
+#     def __str__(self):
+#         return f'{self.name}'
+#
+#     def __bool__(self):
+#         return list(self.name)[-1] not in ('a', 'e', 'o', 'u', 'i')
+#
+#
+# p1 = City('new york')
+# print(p1)  # печатает "New York"
+# print(bool(p1))  # печатает "True"
+# p2 = City('SaN frANCISco')
+# print(p2)  # печатает "San Francisco"
+# print(p2 == True)  # печатает "False"
+class Quadrilateral:
+    def __init__(self, *args):
+        if len(list(args)) == 1:
+            self.width = args[0]
+            self.height = args[0]
+
+        elif len(list(args)) == 2:
+            self.width = args[0]
+            self.height = args[1]
 
     def __str__(self):
-        return f'{self.name}'
+        if self.width == self.height:
+            return f'Куб размером {self.width}х{self.height}'
+        else:
+            return f'Прямоугольник размером {self.width}х{self.height}'
 
     def __bool__(self):
-        return list(self.name)[-1] not in ('a', 'e', 'o', 'u', 'i')
+        return self.width == self.height
 
 
-p1 = City('new york')
-print(p1)  # печатает "New York"
-print(bool(p1))  # печатает "True"
-p2 = City('SaN frANCISco')
-print(p2)  # печатает "San Francisco"
-print(p2 == True)  # печатает "False"
+q1 = Quadrilateral(10)
+print(q1)  # печатает "Куб размером 10х10"
+print(bool(q1))  # печатает "True"
+q2 = Quadrilateral(3, 5)
+print(q2)  # печатает "Прямоугольник размером 3х5"
+print(q2 == True)  # печатает "False"
