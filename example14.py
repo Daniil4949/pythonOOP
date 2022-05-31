7d13cff77c9c277ea48405ddd4144b930d36336c
Daniil Kimstach <daniilkimstachp@gmail.com>
2022-05-31 10:28:56 +0300

    Method super() was used.

--- example14.py
+++ example14.py
@@ -89,20 +89,20 @@
 # print(emp2.get_name(), emp2.is_employee())  # gena bukin True
 
 
-class NewInt(int):
-
-    def repeat(self, k=2):
-        return int(str(self) * k)
-
-    def to_bin(self):
-        return int(bin(self)[2:])
-
-
-a = NewInt(9)
-print(a.repeat())  # печатает число 99
-d = NewInt(a + 5)
-print(d.repeat(3)) # печатает число 141414
-b = NewInt(NewInt(7) * NewInt(5))
-print(b.to_bin()) # печатает 100011 - двоичное представление числа 35
+# class NewInt(int):
+#
+#     def repeat(self, k=2):
+#         return int(str(self) * k)
+#
+#     def to_bin(self):
+#         return int(bin(self)[2:])
+#
+#
+# a = NewInt(9)
+# print(a.repeat())  # печатает число 99
+# d = NewInt(a + 5)
+# print(d.repeat(3)) # печатает число 141414
+# b = NewInt(NewInt(7) * NewInt(5))
+# print(b.to_bin()) # печатает 100011 - двоичное представление числа 35
 
 # Кстати, как вы думаете, что вернет данный вызов NewInt() ?
--- example15.py
+++ example15.py
@@ -1,14 +1,100 @@
+# class Vehicle:
+#     pass
+#
+# class Car(Vehicle):
+#     pass
+#
+# class Plane(Vehicle):
+#     pass
+#
+# class Boat(Vehicle):
+#     pass
+#
+# class RaceCar(Car):
+#     pass
+
+
+# class Person:
+#     def __init__(self, name, surname):
+#         self.name = name
+#         self.surname = surname
+#
+#     def __str__(self):
+#         return f'{self.name} {self.surname}'
+#
+#     def info(self):
+#         print('parent class')
+#         print(self)
+#
+#     def breathe(self):
+#         print('Человек дышит')
+#
+#
+# class Doctor(Person):
+#     def __init__(self, name, surname, age):
+#         super().__init__(name, surname)
+#         self.age = age
+#
+#     def info(self):
+#         su
+
+
+# class Person:
+#     def __init__(self, name, passport):
+#         self.name = name
+#         self.passport = passport
+#
+#     def display(self):
+#         print(f'{self.name}: {self.passport}')
+#
+#
+# class Employee(Person):
+#     def __init__(self, name, passport, salary, department):
+#         super().__init__(name, passport)
+#         self.salary = salary
+#         self.department = department
+#
+#
+# a = Employee('Raul', 886012, 200000, "QA")
+#
+# a.display()  # печатает "Raul: 886012"
+
+
 class Vehicle:
-    pass
+    def __init__(self, name, mileage, capacity: (int, float)):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
 
-class Car(Vehicle):
-    pass
+    def fare(self):
+        return self.capacity * 100
 
-class Plane(Vehicle):
-    pass
+    def display(self):
+        print(f'{self.name} fare is:', self.fare())
 
-class Boat(Vehicle):
-    pass
 
-class RaceCar(Car):
-    pass
+class Bus(Vehicle):
+    def __init__(self, name, mileage):
+        super().__init__(name, mileage, 50)
+
+
+    def fare(self):
+        return super().fare() * 1.1
+
+
+class Taxi(Vehicle):
+    def __init__(self, name, mileage):
+        super().__init__(name, mileage, 4)
+
+    def fare(self):
+        return super().fare() * 1.35
+
+
+sc = Vehicle('Scooter', 100, 2)
+sc.display()
+
+merc = Bus("Mercedes", 120000)
+merc.display()
+
+polo = Taxi("Volkswagen Polo", 15000)
+polo.display()
