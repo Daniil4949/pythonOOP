c0dea88f23d6127ba7d881b40b93a49a6c53d2cd
Daniil Kimstach <daniilkimstachp@gmail.com>
2022-06-04 13:01:31 +0300

    __iter__ and __next__ were added.

--- example20.py
+++ example20.py
@@ -22,20 +22,14 @@
 #     print(i, end=' ')
 
 class PowerTwo:
-    def __init__(self, n):
+    def __init__(self):
         self.index = 0
-        self.power = n
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.index > self.power:
-            raise StopIteration
-        number = 2 ** self.index
-        self.index += 1
+        number = self.index
+        self.index += 10
         return number
 
-w = PowerTwo(4)
-for i in w:
-    print(i)
