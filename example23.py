b6d3528e213d5726242c3044df88e5c5ad35448c
Daniil Kimstach <daniilkimstachp@gmail.com>
2022-06-12 09:43:47 +0300

    Bubble sort was added.

---
+++ example23.py
@@ -0,0 +1,15 @@
+def bubbleSort(arr):
+    n = len(arr)
+    swapped = False
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                swapped = True
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    if not swapped:
+        return
+
+
+arr = [9, 8, 6, 5, 3, 2, 1]
+bubbleSort(arr)
+print(arr)
