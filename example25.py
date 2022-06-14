f46ab5e64d076c1fd18246477193095590ac90a9
Daniil Kimstach <daniilkimstachp@gmail.com>
2022-06-14 16:53:43 +0300

    New easy task from codewars was done.

--- example25.py
+++ example25.py
@@ -9,5 +9,15 @@ def digital_root(n):
 print(digital_root(132189))
 print(str(3333))
 
-# def digital_root(n):
-#     return n if n < 10 else digital_root(sum(map(int,str(n))))
+def digital_root(n):
+    return n if n < 10 else digital_root(sum(map(int,str(n))))
+
+def binary_array_to_number(arr):
+    result = 0
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            result += 2 ** (len(arr) - 1 - i)
+    return result
+
+
+print(binary_array_to_number([ 1, 0, 1]))
